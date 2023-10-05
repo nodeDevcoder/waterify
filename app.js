71e5968ffceb415dc18005e518f5a54866ce9e1b
Jibril Asif <jibrilasif@gmail.com>
2023-10-04 19:09:21 -0700

    changed localhost -> 127.0.0.1"

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.use(methodOverride('_method'));
 
 app.use(morgan('combined'));
 
-mongoose.connect(process.env.DB_URL || 'mongodb://localhost:27017/congressional')
+mongoose.connect(process.env.DB_URL || 'mongodb://127.0.0.1:27017/congressional')
     .then(() => console.log('Connected to DB!'))
     .catch((error) => console.log(error.message));
 
